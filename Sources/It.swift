86fad7b59eefb5a4662c625028f2d224d30bb721
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2020-12-22 09:49:32 +0100

    Modernize the implementation

---
+++ Sources/It.swift
@@ -0,0 +1,43 @@
+// It.swift
+// Copyright © 2020 Adrian Kashivskyy. All rights reserved.
+
+import Quick
+import SwiftCheck
+
+/// Wrap a SwiftCheck `Property` in a Quick example. This makes it possible to
+/// write property tests inside Quick specs.
+///
+/// ```
+/// sc_it("integer addition is commutative") {
+///     forAll { (a: Int, b: Int) in
+///         expect(a + b).sc_to(equal(b + a))
+///     }
+/// }
+/// ```
+///
+/// - Parameters:
+///     - description: Description of the example.
+///     - arguments: Checker arguments for replaying tests.
+///     - flags: Flags to filter examples or example groups.
+///     - closure: Closure containing a property test.
+public func sc_it(_ description: String, arguments: CheckerArguments? = nil, flags: FilterFlags = [:], file: FileString = #file, line: UInt = #line, _ closure: @escaping () -> Property) {
+    it(description, flags: flags, file: file, line: line) {
+        property(description, arguments: arguments, file: file, line: line) <- closure()
+    }
+}
+
+/// Wrap a SwiftCheck `Property` in a focused Quick example. This makes it
+/// possible to write property tests inside Quick specs.
+public func sc_fit(_ description: String, arguments: CheckerArguments? = nil, flags: FilterFlags = [:], file: FileString = #file, line: UInt = #line, _ closure: @escaping () -> Property) {
+    fit(description, flags: flags, file: file, line: line) {
+        property(description, arguments: arguments, file: file, line: line) <- closure()
+    }
+}
+
+/// Wrap a SwiftCheck `Property` in a pending Quick example. This makes it
+/// possible to write property tests inside Quick specs.
+public func sc_xit(_ description: String, arguments: CheckerArguments? = nil, flags: FilterFlags = [:], file: FileString = #file, line: UInt = #line, _ closure: @escaping () -> Property) {
+    xit(description, flags: flags, file: file, line: line) {
+        property(description, arguments: arguments, file: file, line: line) <- closure()
+    }
+}
